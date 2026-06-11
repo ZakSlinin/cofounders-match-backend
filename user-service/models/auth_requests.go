ab4532b40f9887bf564b497280075c6d1abe5f54
Zak Slinin <156526564+ZakSlinin@users.noreply.github.com>
2026-06-11 23:08:59 +0300

    feat: auth requests models

--- user-service/models/auth_requests.go
+++ user-service/models/auth_requests.go
@@ -5,3 +5,12 @@ type RegisterRequest struct {
 	Password string `json:"password" binding:"required,min=8"`
 	Role     string `json:"role" binding:"required"`
 }
+
+type LoginRequest struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=8"`
+}
+
+type RefreshTokenRequest struct {
+	RefreshToken string `json:"refresh_token" binding:"required"`
+}
