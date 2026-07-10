788e559e43d090433342841e9ad13475795fe087
Zak Slinin <156526564+ZakSlinin@users.noreply.github.com>
2026-07-10 14:10:15 +0300

    fix: remove action like from func CheckMutalLike

--- match-service/swipe/swipe-repository/swipe-repository.go
+++ match-service/swipe/swipe-repository/swipe-repository.go
@@ -24,7 +24,7 @@ func (r *SwipeRepository) CreateSwipe(ctx context.Context, fromUser, toUser stri
 func (r *SwipeRepository) CheckMutualLike(ctx context.Context, fromUser, toUser string) (bool, error) {
 	var count int64
 	err := r.db.WithContext(ctx).Model(&swipe.Swipe{}).
-		Where("from_user = ? AND to_user = ? AND action = 'like'", toUser, fromUser).
+		Where("from_user = ? AND to_user = ?", toUser, fromUser).
 		Count(&count).Error
 	return count > 0, err
 }
